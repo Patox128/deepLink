ba3418189d557b625923c9f7d33b6232503b366a
Patricio Mora Vega <patricio.mora99@gmail.com>
2024-01-04 17:31:27 -0300

    Update DEEPLINK URL in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <a href="https://preapp.rayensalud.com/recovery/123">DEEPLINK</a>
+      <a href="tintegra://preapp.rayensalud.com/recovery/123">DEEPLINK</a>
     </>
   );
 }
